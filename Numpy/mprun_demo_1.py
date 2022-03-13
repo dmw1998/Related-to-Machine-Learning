9237c3390e4dce2ef33fadf0b63f77348df7a9af
Miaowen <66027205+dmw1998@users.noreply.github.com>
2022-03-13 18:50:29 +0900

    Numpy

---
+++ Numpy/mprun_demo_1.py
@@ -0,0 +1,9 @@
+
+@profile
+def my_func():
+    n = int(1e+8) 
+    temp = [i for i in range(n)]
+    return temp
+
+if __name__ == '__main__':
+    my_func()
---
+++ Numpy/mprun_demo_2.py
@@ -0,0 +1,4 @@
+import numpy as np
+def test_2(n):
+    temp = np.arange(n)
+    return temp
